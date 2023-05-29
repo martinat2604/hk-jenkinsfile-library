c121cc51450d38cdc967119d02dd0598cd706486
hemanth <hemanth.velivela@serole.com>
2023-05-29 22:39:45 +0530

    git checkout code added

---
+++ vars/pipeline_ci_cd.groovy
@@ -0,0 +1,17 @@
+pipeline{
+
+    agent any
+
+    stages{
+         
+        stage('Git Checkout'){
+          when { expression {  params.action == 'create' } }
+            steps{
+            gitCheckout(
+                branch: "master",
+                url: "https://github.com/martinat2604/hk-test-java-app.git"
+            )
+          }
+        }
+    }
+}
