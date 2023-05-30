b00e3ccebfd945a2e20bf1608e8fff5c3acfa9bb
hemanth <hemanth.velivela@serole.com>
2023-05-30 11:36:31 +0530

    testing code added in the  repo

--- vars/pipelineGitFlow.groovy
+++ vars/pipelineGitFlow.groovy
@@ -6,10 +6,12 @@ pipeline{
          
         stage('Git Checkout'){
             steps{
-            gitCheckout(
-                branch: "master",
-                url: "https://github.com/martinat2604/hk-test-java-app.git"
-            )
+              checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/martinat2604/hk-test-java-app.git']])
+
+            // gitCheckout(
+            //     branch: "master",
+            //     url: "https://github.com/martinat2604/hk-test-java-app.git"
+            // )
           }
         }
     }
