966a1e474c3c2273f412aeff973fc5c7da18ce92
hemanth <hemanth.velivela@serole.com>
2023-05-30 11:43:05 +0530

    testing code added in the  repo

--- vars/libgitCheckOut.groovy
+++ vars/libgitCheckOut.groovy
@@ -1,7 +1,7 @@
 def call(Map stageParams) {
  
 checkout scmGit(
-    $class: 'GitSCM',
+     
     branches: [[name: stageParams.branch]],
     extensions: [], 
     userRemoteConfigs: [[url: stageParams.url]])
