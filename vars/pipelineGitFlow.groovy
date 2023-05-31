6bfa4251bce4e5c975216776e2147d200875b614
hemanth <hemanth.velivela@serole.com>
2023-05-31 14:59:31 +0530

    git checkout code modified in shared libraries

--- vars/pipelineGitFlow.groovy
+++ vars/pipelineGitFlow.groovy
@@ -1,28 +1,24 @@
 def call(body) {
-  
-def pipelineParams = [:]    
-body.resolveStrategy = Closure.DELEGATE_FIRST
-body()
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body()
     def jobNameParts = env.JOB_NAME.tokenize('/') as String[]
     env.JOB_CONSOLE_NAME =  (jobNameParts.length < 2 ) ? env.JOB_NAME : jobNameParts[jobNameParts.length - 2]
 
-pipeline{
-    agent any
-    stages{
-
-// Here we will call the  git checkout library.groovy.    
-        stage('Git Checkout'){
-            steps{
-            libgitCheckOut(
-                branch: "$GIT_BRANCH",
-                url: "$GIT_URL"
-            )
-          }
+    pipeline {
+        agent any
+        stages {
+            // Here we will call the  git checkout library.groovy.
+            stage('Git Checkout') {
+                steps {
+                    script(
+                        libgitCheckOut(
+                            branch: "$GIT_BRANCH",
+                            url: "$GIT_URL"
+                        )
+                    )
+                }
+            }
         }
     }
-  }
 }
-
-
-
- 
