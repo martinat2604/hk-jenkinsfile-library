e47ab0dac616efbe26c0c3c12e2c107d2e808dde
hemanth <hemanth.velivela@serole.com>
2023-06-02 17:35:49 +0530

    slack code modified

--- vars/libNotificationSlack.groovy
+++ vars/libNotificationSlack.groovy
@@ -1,44 +1,47 @@
-// def call(String buildStatus) {
+def call (String channel='java') {
 
-//     // build status of null means successful
-//     buildStatus = buildStatus ?: 'SUCCESS'  // if the buildStatus variable is null or undefined, it will be assigned the value 'SUCCESS'. If buildStatus already has a value (not null or undefined), it will retain that value.
+  env.BUILD_RESULT = currentBuild.result ?: 'SUCCESS'
 
-//     // Default values
-//     def color = 'RED'
-//     def colorCode = '#FF0000'
-//     def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-//     def summary = "${subject} (${env.BUILD_URL})"
-//     // def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-//     // <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-
-//   if ( buildStatus == "SUCCESS" ) {
-//     slackSend channel: 'java', color: "good", failOnError: true, notifyCommitters: true, teamDomain: 'optus-nzp9955', tokenCredentialId: 'slack-integration-1', factDefinitions: [[name: "Job Name", template: "${JOB_CONSOLE_NAME}"],[name: "Branch Name", template: "${BRANCH_NAME}"],[name: "Build ID", template: "${BUILD_ID}"],[name: "Version", template: "${VERSION}"]]
-//   }
-//   else if( buildStatus == "FAILURE" ) { 
-//     slackSend channel: 'java', color: "danger", failOnError: true, notifyCommitters: true, teamDomain: 'optus-nzp9955', tokenCredentialId: 'slack-integration-1', factDefinitions: [[name: "Job Name", template: "${JOB_CONSOLE_NAME}"],[name: "Branch Name", template: "${BRANCH_NAME}"],[name: "Build ID", template: "${BUILD_ID}"],[name: "Version", template: "${VERSION}"]]
-//   }
-//   else if( buildStatus == "UNSTABLE" ) { 
-//     slackSend channel: 'java', color: "warning", failOnError: true, notifyCommitters: true, teamDomain: 'optus-nzp9955', tokenCredentialId: 'slack-integration-1', factDefinitions: [[name: "Job Name", template: "${JOB_CONSOLE_NAME}"],[name: "Branch Name", template: "${BRANCH_NAME}"],[name: "Build ID", template: "${BUILD_ID}"],[name: "Version", template: "${VERSION}"]]
-//   }
-//   else {
-//     slackSend channel: 'java', color: "good", failOnError: true, notifyCommitters: true, teamDomain: 'optus-nzp9955', tokenCredentialId: 'slack-integration-1', factDefinitions: [[name: "Job Name", template: "${JOB_CONSOLE_NAME}"],[name: "Branch Name", template: "${BRANCH_NAME}"],[name: "Build ID", template: "${BUILD_ID}"],[name: "Version", template: "${VERSION}"]]
-//   }
-// }
-
-
-
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  //if (env.BUILD_RESULT == 'SUCCESS') {
+  //if (['UNSTABLE', 'ABORTED'].contains(env.BUILD_RESULT)) {
+  switch (env.BUILD_RESULT) {
+    case 'SUCCESS':
+      env.SLACK_COLOR = 'good'
+      env.BUILD_RESULT = env.BUILD_RESULT.toLowerCase().capitalize()
+      break
+    // fallthrough to set warning for either of these conditions
+    case 'UNSTABLE':
+    case 'ABORTED':
+      env.SLACK_COLOR = 'warning'
+    default:
+      env.SLACK_COLOR = 'danger'
   }
-  else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+
+  env.SLACK_USERTAGS = ''
+  if (env.SLACK_COLOR != 'good') {
+    env.SLACK_USERTAGS = slackBrokenCommitters().join(' ')
   }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  if (env.SLACK_USERTAGS) {
+    env.SLACK_USERTAGS = '- ' + env.SLACK_USERTAGS
   }
-  else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-  }
+
+  // $BUILD_URL       for Classic UI
+  // $RUN_DISPLAY_URL for Blue Ocean UI
+  env.SLACK_LINKS  = "Pipeline <$JOB_DISPLAY_URL|$JOB_NAME> - <$RUN_DISPLAY_URL|Build #$BUILD_NUMBER>"
+  env.SLACK_MESSAGE = "Job $BUILD_RESULT - $SLACK_LINKS $SLACK_USERTAGS"
+
+  slackSend (
+    // works but channels seem to work with or without # prefix, making this unnecessary
+    //channel: "#${channel.replaceAll('^#+', '')}",
+    channel: "$channel",
+    color: "$SLACK_COLOR",
+    message: "$SLACK_MESSAGE",
+    notifyCommitters: true,
+
+    teamDomain: 'optus-nzp9955',
+    tokenCredentialId: 'slack-integration-1'
+    // notifyCommitters requires bot user
+    botUser: true,
+    username: 'Jenkins'  // override display name
+  )
 }
- 
